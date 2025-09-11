b795d4a9aa812a399b7baabbc476ea73b66e3f5d
AlaaRef3t <alaarefaat889@gmail.com>
2025-09-11 17:30:25 +0300

    -_-

--- src/lib/token.utils.ts
+++ src/lib/token.utils.ts
@@ -1,28 +1,16 @@
-// src/lib/token.utils.ts
-import { decode, type JWT } from "next-auth/jwt";
+import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-const SECRET = process.env.NEXTAUTH_SECRET ?? process.env.AUTH_SECRET!;
-
-type MyJWT = JWT & {
-  accessToken?: string;
-  token?: string;
-};
-
 export async function getUserToken() {
-  const jar = await cookies(); // في بيئتك محتاجة await
+    
 
-  const encodedToken =
-    jar.get("__Secure-next-auth.session-token")?.value ||
-    jar.get("next-auth.session-token")?.value;
+        const encodedToken = (await cookies()).get("next-auth.session-token")?.value
 
-  if (!encodedToken || !SECRET) return null;
+        const decToken = await decode({ token: encodedToken, secret: process.env.AUTH_SECRET! })
+        console.log(decToken, "tokeeeeeeeeeeeeeeeeeeeeeeeeeeen");
+        
+        const token = decToken?.token
 
-  const payload = (await decode({
-    token: encodedToken,
-    secret: SECRET,
-  })) as MyJWT | null;
 
-  // خُد التوكن من أي مفتاح بتضيفه في callbacks
-  return payload?.accessToken ?? payload?.token ?? null;
+    return token
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,7 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    const token =
-    request.cookies.get('__Secure-next-auth.session-token')?.value ||
-    request.cookies.get('next-auth.session-token')?.value;
+    const token = request.cookies.get("next-auth.session-token")
 
     if (!token) {
 
