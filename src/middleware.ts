8163239e209a0d6c7269e8649997355e99466eca
AlaaRef3t <alaarefaat889@gmail.com>
2025-09-11 16:42:46 +0300

    fixed middleware token

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,9 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    const token = request.cookies.get("next-auth.session-token")
+    const token =
+    request.cookies.get('__Secure-next-auth.session-token')?.value ||
+    request.cookies.get('next-auth.session-token')?.value;
 
     if (!token) {
 
